6256f3d87e13658d4b3c39c366ee2f160771f860
usoysal <us.soysal@gmail.com>
2020-11-11 22:29:02 -0600

    clean-up

--- util.py
+++
@@ -1,7 +0,0 @@
-
-from boat import *
-
-
-def initial():
-	b=boat(boat.state(lat=2,lon=4))
-	return b
--- worldmap2.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib as mlt
-from mpl_toolkits.basemap import Basemap
-from PIL import Image
-from pylab import rcParams
